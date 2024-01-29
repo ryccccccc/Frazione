ab8e7647b1ec5fa2e3c9b404c1bf4261f3b30450
Riccardo Nervosi <riccardo.nervosi@gmail.com>
2024-01-29 15:05:44 +0100

    Aggiungere i file di progetto.

---
+++ Frazione/Frazione.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Frazione
+    {
+        public int Numeratore { get; set; }
+        public int Denominatore { get; set; }
+        public Frazione(int num, int denomin) 
+        {
+            Numeratore = num;
+            Denominatore = denomin;
+            Semplificatore(ref num, ref denomin);
+        }
+        private static int MCD(int a, int b)
+        {
+            int r; 
+            while (b != 0) 
+            {
+                r = a % b; 
+                a = b; 
+                b = r; 
+            }
+            return a;
+        }
+
+        public static void Semplificatore(ref int num, ref int denomin)
+        {
+            int divisore = MCD(num, denomin);
+            num = num / divisore;
+            denomin = denomin / divisore;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numeratore, Denominatore);
+        }
+
+        public static Frazione Parse(string fraz)
+        {
+            string[] termini = fraz.Split('/');
+            Frazione f = new Frazione(int.Parse(termini[0]), int.Parse(termini[1]));
+            return f;
+        }
+    }
+}
---
+++ Frazione/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frazione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione f = new Frazione(5,25);
+            f.ToString();
+            Console.WriteLine(f);
+
+            Frazione fp = Frazione.Parse(f.ToString());
+            Console.WriteLine(fp);
+
+            Console.ReadLine();
+        }
+    }
+}
